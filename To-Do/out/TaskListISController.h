8c12d5d15cc3c7c7e65f7a321342109b5a02d136
Mats Sandvoll <matssandvoll@gmail.com>
2013-10-28 12:48:05 +0000

    Minor changed

--- To-Do/out/TaskListISController.h
+++ To-Do/out/TaskListISController.h
@@ -1,11 +1,11 @@
 
 #import <UIKit/UIKit.h>
-#import "ViewTaskISController.h"
 #import "CreateNewTaskISController.h"
+#import "ViewTaskISController.h"
 #import "Task.h"
 
 
-@interface TaskListISController : UIViewController <ViewTaskISControllerDelegate,CreateNewTaskISControllerDelegate, UITableViewDataSource, UITableViewDelegate>
+@interface TaskListISController : UIViewController <CreateNewTaskISControllerDelegate,ViewTaskISControllerDelegate, UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic, retain) UITableView *tableView;
 @property (nonatomic, retain) NSMutableArray *taskArray;
